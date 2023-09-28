21170058d18f828e3de99af6af0792d156123666
memintan <memintan@gmail.com>
2023-09-27 22:41:10 -0500

    classes are done

--- fourth/dictionary.py
+++ fourth/dictionary.py
@@ -3,18 +3,18 @@ employee1 = {}
 employee1['name'] = 'james'
 print(employee1)
 
-print('------------------------------------')
+print('1------------------------------------')
 
 employee1['name'] = 'daniel'
 print(employee1)
 
-print('------------------------------------')
+print('2------------------------------------')
 
 employee1['age'] = 45
 employee1['salary'] = 60_000
 print(employee1)
 
-print('------------------------------------')
+print('3------------------------------------')
 
 employee2 = {
     'name': 'James',
@@ -26,28 +26,28 @@ employee2 = {
 print(employee2)
 print(employee2['name'])
 
-print('------------------------------------')
+print('4------------------------------------')
 
 employee2['salary'] += 10000
 print(employee2)
 
-print('------------------------------------')
+print('5------------------------------------')
 
 employee2.update({'age': 40})
 print(employee2)
 
-print('------------------------------------')
+print('6------------------------------------')
 
 employee2['full_time'] = True
 # employee2.update({'full_time': True}) ____same
 print(employee2)
 
-print('------------------------------------')
+print('7------------------------------------')
 
 employee2.pop('full_time')
 print(employee2)
 
-print('------------------------------------')
+print('8------------------------------------')
 
 employee2.popitem()
 print(employee2)
@@ -73,12 +73,12 @@ print('------------------------------------')
 values = employee3.values()
 print(values)
 
-print('------------------------------------')
+print('items------------------------------------')
 
 for x in employee3.items():
     print(f'{x[0]} : {x[1]}')
 
-print('------------------------------------')
+print('students------------------------------------')
 
 students = {
     'A01': {
@@ -122,13 +122,10 @@ print(students['A03'])
 
 print('------------------------------------')
 
-
-
-print('------------------------------------')
-
 for value in students.values():
-    print(value)
+    # print(value)
+    print('===============')
     for item in value.items():
-        print(item[1])
+        print(f'{item[0]} => {item[1]}')
 
 print('------------------------------------')
